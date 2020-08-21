87dfdb3153b97478c02332c5c1d3c7bbf0c890ee
LiYaMei94 <1016112504@qq.com>
2020-08-21 15:08:32 +0800

    '初始化'

---
+++ src/components/cartPanel.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="cart_panel"></div>
+</template>
+
+<script>
+export default {
+  name: "CartPanel",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped></style>
